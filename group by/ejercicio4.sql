b1f970db7a4ad45837dd694c17f9dac0cdcea491
Jorge Ramos <jorgeramosings@outlook.com>
2026-08-30 19:16:08 -0500

    group by AB24

--- group by/ejercicio4.sql
+++ group by/ejercicio4.sql
@@ -1,3 +1,3 @@
 --El área quiere analizar los siniestros por estado. Necesitas mostrar, por cada estado, cuántos siniestros existen y cuál es el promedio reclamado,
 -- pero solo considerando reclamaciones superiores a 1000000.
-select estado, count(*) as cantidad_siniestros, avg(valor_reclamado) as valor from siniestros where valor_reclamado > 1000000 GROUP BY estado;
+select estado, count(*) as cantidad_siniestros, CAST(avg(valor_reclamado) as INTEGER) as Valor from siniestros where valor_reclamado > 1000000 GROUP BY estado;
